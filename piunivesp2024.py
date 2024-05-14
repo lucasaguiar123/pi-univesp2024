8b8fa265f3985c106ec0ad95572210d09ad9ffe6
lucasaguiar123 <161175900+lucasaguiar123@users.noreply.github.com>
2024-05-13 21:43:44 -0300

    Update piunivesp2024.py

--- piunivesp2024.py
+++ piunivesp2024.py
@@ -1,41 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request
 import psycopg2
 from psycopg2 import OperationalError, Error
+import vercel
 
 app = Flask(__name__,template_folder='templates')
 
 # Configuração da conexão com o banco de dados PostgreSQL
-DATABASE = {
-    'dbname': 'estoque',
-    'user': 'postgres',
-    'password': 'mudar123',
-    'host': 'localhost',
-    'port': '5432'
-}
+DATABASE_URL = 'postgres://default:7Vxa6mIUDeop@ep-ancient-shape-a4mn5jxw-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
   
-# Função para criar a tabela de estoque no PostgreSQL
-def create_table():
-    try:
-        conn = psycopg2.connect(**DATABASE)
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS estoque (
-                          id SERIAL PRIMARY KEY,
-                          nome TEXT NOT NULL,
-                          quantidade INTEGER NOT NULL,
-                          preco REAL NOT NULL
-                          )''')
-        conn.commit()
-        conn.close()
-    except (OperationalError, Error) as e:
-        print(f"Erro ao criar a tabela de estoque: {e}")
-
 # Rota principal para exibir o estoque
 @app.route('/',methods=['GET'])
 def estoque():
     try:
-        conn = psycopg2.connect(**DATABASE)
+        conn = psycopg2.connect(DATABASE_URL)
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM estoque')
+        cursor.execute('SELECT * FROM estoque_padua')
         items = cursor.fetchall()
         conn.close()
         return render_template('estoque.html', items=items)
@@ -51,31 +30,30 @@ def adicionar_item():
         quantidade = request.form['quantidade']
         preco = request.form['preco']
         try:
-            conn = psycopg2.connect(**DATABASE)
+            conn = psycopg2.connect(DATABASE_URL)
             cursor = conn.cursor()
-            cursor.execute('INSERT INTO estoque (nome, quantidade, preco) VALUES (%s, %s, %s)', (nome, quantidade, preco))
+            cursor.execute("INSERT INTO estoque_padua (nome, quantidade, preco) VALUES ( %s, %s, %s)", (nome, quantidade, preco))
             conn.commit()
-            conn.close()
-            return redirect(url_for('/'))
+            conn.close()       
         except (OperationalError, Error) as e:
             print(f"Erro ao adicionar item ao estoque: {e}")
-            return redirect(url_for('/'))
+            return redirect(url_for('/estoque_padua'))
     return render_template('adicionar_item.html')
 
 # Rota para excluir um item do estoque
 @app.route('/excluir/<int:item_id>')
 def excluir_item(item_id):
     try:
-        conn = psycopg2.connect(**DATABASE)
+        conn = psycopg2.connect(DATABASE_URL)
         cursor = conn.cursor()
-        cursor.execute('DELETE FROM estoque WHERE id = %s', (item_id,))
+        cursor.execute('DELETE FROM estoque_padua WHERE id_produto = %s', (item_id,))
         conn.commit()
         conn.close()
-        return redirect(url_for('estoque'))
+        
     except (OperationalError, Error) as e:
         print(f"Erro ao excluir item do estoque: {e}")
-        return redirect(url_for('estoque'))
+        return redirect(url_for('/estoque_padua'))
+    return render_template('adicionar_item.html')
 
 if __name__ == '__main__':
-    create_table()  # Garante que a tabela de estoque esteja criada antes de iniciar o servidor Flask
     app.run(debug=True)
